49178b36aac45459bde06898c5947e2a55cd6e4e
imamura <imamuray1313@gmail.com>
2019-04-11 19:59:54 +0900

    ステップ5　可変長ベクタのデータ構造ユニットテスト

--- 9cc.c
+++ 9cc.c
@@ -27,8 +27,17 @@ typedef struct Node {
   int val;          // tyがND_NUMの場合のみ使う
 } Node;
 
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
 Token tokens[MAXTK];
 int pos = 0;
+
+Vector *new_vector();
+void vec_push(Vector *vec, void *elem);
 
 void tokenize(char *p);
 Node *new_node(int ty, Node *lhs, Node *rhs);
@@ -51,10 +60,38 @@ void error(char *s) {
   exit(1);
 }
 
+void expect(int line, int expected, int actual) {
+  if (expected == actual)
+    return;
+  fprintf(stderr, "%d: %d expected, but got %d\n",
+          line, expected, actual);
+  exit(1);
+}
+
+void runtest() {
+  Vector *vec = new_vector();
+  expect(__LINE__, 0, vec->len);
+
+  for (int i = 0; i < 100; i++)
+    vec_push(vec, (void *)i);
+
+  expect(__LINE__, 100, vec->len);
+  expect(__LINE__, 0, (int)vec->data[0]);
+  expect(__LINE__, 50, (int)vec->data[50]);
+  expect(__LINE__, 99, (int)vec->data[99]);
+
+  printf("OK\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
+  }
+
+  if (strncmp(argv[1], "-test", 5) == 0) {
+    runtest();
+    exit(0);
   }
 
   // トークナイズしてパースする
@@ -75,6 +112,22 @@ int main(int argc, char **argv) {
   printf("  pop rax\n");
   printf("  ret\n");
   return 0;
+}
+
+Vector *new_vector() {
+  Vector *vec = malloc(sizeof(Vector));
+  vec->data = malloc(sizeof(void *) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+  return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+  }
+  vec->data[vec->len++] = elem;
 }
 
 // pが指している文字列をトークンに分割してtokensに保存する
