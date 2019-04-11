0ba75cb1d2d86128ab23f0067e6290d2104620f8
imamura <imamuray1313@gmail.com>
2019-04-11 18:55:55 +0900

    ステップ4　*の確認

--- 9cc.c
+++ 9cc.c
@@ -87,7 +87,9 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' ||
+        *p == '*' || *p == '/' ||
+        *p == '(' || *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
